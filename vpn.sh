2b37d3d3d6ee22c46d27cbf32c5c041e2318b85b
Adrian Moreno <adrian@morenomartinez.com>
2016-08-19 12:42:01 +0200

    Fixes indentation to match with the original format

--- vpn.sh
+++ vpn.sh
@@ -17,8 +17,8 @@
 
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root or sudo " 1>&2
-   exit 1
+	 echo "This script must be run as root or sudo " 1>&2
+	 exit 1
 fi
 
 
@@ -29,85 +29,85 @@ DIR=`dirname $0`
 
 if [ -z "$COMMAND" ]
 then
-  echo "COMMAND not specified - (C)onnect or (D)isconnect"
-  exit 1
+	echo "COMMAND not specified - (C)onnect or (D)isconnect"
+	exit 1
 fi
 
 
 if [ "$COMMAND" == "C" ]
 then
-  if [ -z "$USERNAME" ]
-  then
-    echo "USERNAME not specified"
-    exit 1
-  fi
+	if [ -z "$USERNAME" ]
+	then
+		echo "USERNAME not specified"
+		exit 1
+	fi
 
-  if [ -z "$LOCATION" ]
-  then
-    LOCATION=$VPNSERVER
-    if [ -z "$LOCATION" ]
-    then
-      echo "VPN endpoint not specified. Please set the VPNSERVER environment variable or pass the endpoint as an argument"
-      exit 1
-    fi
-  fi
+	if [ -z "$LOCATION" ]
+	then
+		LOCATION=$VPNSERVER
+		if [ -z "$LOCATION" ]
+		then
+			echo "VPN endpoint not specified. Please set the VPNSERVER environment variable or pass the endpoint as an argument"
+			exit 1
+		fi
+	fi
 
-  echo "Checking for openconnect binary..."
-  if [ ! -e /usr/local/bin/openconnect ]
-  then
-    echo "openconnect binary not found, install it with homebrew (http://brew.sh)"
-    exit 1
-  fi
+	echo "Checking for openconnect binary..."
+	if [ ! -e /usr/local/bin/openconnect ]
+	then
+		echo "openconnect binary not found, install it with homebrew (http://brew.sh)"
+		exit 1
+	fi
 
 
-  echo "Checking for cstub binary..."
-  if [ ! -e /opt/cisco/hostscan/bin/cstub ]
-  then
-    echo "cstub binary not found, get a full copy of the anyconnect installer and add the 'Posture' component..."
-    exit 1
-  fi
+	echo "Checking for cstub binary..."
+	if [ ! -e /opt/cisco/hostscan/bin/cstub ]
+	then
+		echo "cstub binary not found, get a full copy of the anyconnect installer and add the 'Posture' component..."
+		exit 1
+	fi
 
-  #echo "Updating Stats"
-  #curl -ss -o /dev/null -fG --data-urlencode sysversion="$(uname -v)" --data-urlencode user="$(echo $SUDO_USER)" --data-urlencode ip="$(curl -ss icanhazip.com)" --data-urlencode euid="$USERNAME)" --data-urlencode appname="vpnsplit2" http://collectappinfo.appspot.com &
-  echo "Running openconnect"
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  openconnect -u $USERNAME --background --timestamp --no-cert-check $LOCATION --csd-wrapper $DIR/csdwrapper.sh --script=$DIR/vpnc-script
-  echo "Checking connection functionality"
-  sleep 1
-  ping -c 4 -i 1 -Q -t 1 -o 10.5.132.1
-  export RESULT=$?
-  if [ $RESULT == 0 ]
-  then
-    echo ""
-    echo "YOU ARE CONNECTED"
-  else
-    echo ""
-    echo "CONNECTION FAILED, CAN'T PING THROUGH TUNNEL.  RESULT:"
-    echo $RESULT
-  fi
-  exit 0
+	#echo "Updating Stats"
+	#curl -ss -o /dev/null -fG --data-urlencode sysversion="$(uname -v)" --data-urlencode user="$(echo $SUDO_USER)" --data-urlencode ip="$(curl -ss icanhazip.com)" --data-urlencode euid="$USERNAME)" --data-urlencode appname="vpnsplit2" http://collectappinfo.appspot.com &
+	echo "Running openconnect"
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	openconnect -u $USERNAME --background --timestamp --no-cert-check $LOCATION --csd-wrapper $DIR/csdwrapper.sh --script=$DIR/vpnc-script
+	echo "Checking connection functionality"
+	sleep 1
+	ping -c 4 -i 1 -Q -t 1 -o 10.5.132.1
+	export RESULT=$?
+	if [ $RESULT == 0 ]
+	then
+		echo ""
+		echo "YOU ARE CONNECTED"
+	else
+		echo ""
+		echo "CONNECTION FAILED, CAN'T PING THROUGH TUNNEL.  RESULT:"
+		echo $RESULT
+	fi
+	exit 0
 fi
 
 if [ "$COMMAND" == "D" ]
 then
-  export TUNDEV=`cat /tmp/tundev`
-  if [ -z "$TUNDEV" ]
-  then
-    echo "TUNDEV not found: DNS will likely be broken"
-    echo "Trying tun0 anyways"
-    export TUNDEV="tun0"
-  fi
-  killall openconnect
-  scutil >/dev/null 2>&1 <<-EOF
-    open
-    remove State:/Network/Service/$TUNDEV/IPv4
-    remove State:/Network/Service/$TUNDEV/DNS
-    close
-  EOF
-  echo "Briefly bouncing en0/en1 to clear cached DNS"
-  sudo ifconfig en0 down && sudo ifconfig en0 up
-  sudo ifconfig en1 down && sudo ifconfig en1 up
-  exit 0
+	export TUNDEV=`cat /tmp/tundev`
+	if [ -z "$TUNDEV" ]
+	then
+		echo "TUNDEV not found: DNS will likely be broken"
+		echo "Trying tun0 anyways"
+		export TUNDEV="tun0"
+	fi
+	killall openconnect
+	scutil >/dev/null 2>&1 <<-EOF
+		open
+		remove State:/Network/Service/$TUNDEV/IPv4
+		remove State:/Network/Service/$TUNDEV/DNS
+		close
+	EOF
+	echo "Briefly bouncing en0/en1 to clear cached DNS"
+	sudo ifconfig en0 down && sudo ifconfig en0 up
+	sudo ifconfig en1 down && sudo ifconfig en1 up
+	exit 0
 fi
 
 echo "Command not properly specified - (C) or (D).  Try again"
